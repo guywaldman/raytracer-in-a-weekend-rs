0e79e60c0f8cd5da8da7844be5be51c4e03cda82
Guy Waldman <guywald@microsoft.com>
2020-07-31 01:00:10 +0300

    Graphics "hello world"

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let width = 256;
+    let height = 256;
+
+    println!("P3");
+    println!("{} {}", width, height);
+    println!("255");
+
+    for j in (0..height).rev() {
+        for i in 0..width {
+            let r = (i as f64) / (f64::from(width) - 1.0);
+            let g = (j as f64) / (f64::from(height) - 1.0);
+            let b = 0.25;
+
+            let r_col = (r * 255.999) as u8;
+            let g_col = (g * 255.999) as u8;
+            let b_col = (b * 255.999) as u8;
+
+            println!("{} {} {}", r_col, g_col, b_col);
+        }
+    }
 }
